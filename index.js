4f254d170d66f1b34fe97d3b528fab1f73918bcf
R S <manik9092526954@gmail.com>
2024-08-28 18:03:39 +0530

    vercel added

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+const productRouter = require("./routes/products");
+const orderRouter = require("./routes/order");
+const userRouter = require("./routes/userRoute");
+const otpRouter = require("./routes/Otp");
+const cartRouter = require("./routes/cart")
+const paymentRouter = require("./routes/payment")
+const ImageRouter = require("./routes/ImageUpload")
+// const {requireSignIn, isAdmin} = require("./middlewares/authMiddleware");
+const morgan = require("morgan");
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use("/api", productRouter);
+app.use("/api", orderRouter);
+app.use("/api", userRouter);
+app.use("/api", otpRouter);
+app.use("/api",cartRouter);
+app.use("/api", paymentRouter);
+app.use("/api", ImageRouter)
+app.get("/test",(req,res)=>{
+  res.send("HI da")
+})
+mongoose
+  .connect(process.env.MONG_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`DB Connected and Served is running on ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
